b450b1fa15f6ac3b262917eedb76012a25344392
melisacar <melisa.acar@ipa.istanbul>
2025-01-28 10:50:23 +0300

    feat: create main src folder and file

---
+++ src/main.py
@@ -0,0 +1,19 @@
+
+
+# Mapping months
+months_mapping = {
+
+    1: "OCAK",
+    2: "ŞUBAT",
+    3: "MART",
+    4: "NİSAN",
+    5: "MAYIS",
+    6: "HAZİRAN",
+    7: "TEMMUZ",
+    8: "AĞUSTOS",
+    9: "EYLÜL",
+    10: "EKİM",
+    11: "KASIM",
+    12: "ARALIK"
+}
+
